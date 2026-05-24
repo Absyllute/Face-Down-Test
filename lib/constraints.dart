e8cffd96900f09b59d7e8d9a6adde9e9b50f04d3
Absyllute <absyllute@gmail.com>
2026-05-24 16:54:26 +0200

    reworked face-down detection

--- lib/constraints.dart
+++ lib/constraints.dart
@@ -1,25 +1,29 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
 class PhoneConstraints {
-  static double target = 10.0;
-  static double leniency = 1;
-  static bool phoneFaceDown = false; 
+  static double target = -10.0;
+  static double enterLeniency = 1.0;
+  static double exitLeniency = 2.5; 
+  static bool phoneFaceDown = false;
   static StreamSubscription? subscription;
 
-  static void startDetecting() {
+  static void startDetecting({VoidCallback? onUpdate}) {
     subscription = accelerometerEventStream(
-      samplingPeriod: SensorInterval.normalInterval
-    ).listen(
-      (AccelerometerEvent event) {
-        if ((event.z - target).abs() <= leniency) {
-          phoneFaceDown = true;
-        } else {
-          phoneFaceDown = false;
-        }
+      samplingPeriod: SensorInterval.normalInterval,
+    ).listen((AccelerometerEvent event) {
+      final distance = (event.z - target).abs();
+
+      if (!phoneFaceDown && distance <= enterLeniency) {
+        phoneFaceDown = true;
+        onUpdate?.call();
+      } else if (phoneFaceDown && distance > exitLeniency) {
+        phoneFaceDown = false;
+        onUpdate?.call();
       }
-    );
+    });
   }
 
   static void stopDetecting() {
